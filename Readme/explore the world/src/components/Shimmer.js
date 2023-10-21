b5623365e629ecde27f3265a518fb938ab9b67cd
Sajid5713 <sajid3115936@gmail.com>
2023-10-21 19:16:25 +0500

    explore the world

---
+++ Readme/explore the world/src/components/Shimmer.js
@@ -0,0 +1,75 @@
+function Shimmer() {
+  return (
+    <>
+     <div className="container">
+        <div className="search-bar">
+            <div className="form">
+                <input type="text" className="textbox" placeholder="Search" style={{background:'#cfcfcf96'}} />
+                <input title="Search" value="⌕" type="submit" className="button" />
+            </div>
+        </div>
+        <div className="filter-nav">
+            <button className="btn">Top Rated ★</button>
+        </div>
+        <div className="res-container">
+        <div className="res-card">
+            <div className="image" style={{background:'#cfcfcf96'}}>
+                <div></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Restaurant Name</h2>
+                <p className="cuisine">Indian, Chinese, Beverages, Sodepur</p>
+                <h4>4.3 ★</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+        <div className="res-card">
+            <div className="image" style={{background:'#cfcfcf96'}}>
+                <div></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Restaurant Name</h2>
+                <p className="cuisine">Indian, Chinese, Beverages, Sodepur</p>
+                <h4>4.3 ★</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+        <div className="res-card">
+            <div className="image" style={{background:'#cfcfcf96'}}>
+                <div></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Restaurant Name</h2>
+                <p className="cuisine">Indian, Chinese, Beverages, Sodepur</p>
+                <h4>4.3 ★</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+        <div className="res-card">
+            <div className="image" style={{background:'#cfcfcf96'}}>
+                <div></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Restaurant Name</h2>
+                <p className="cuisine">Indian, Chinese, Beverages, Sodepur</p>
+                <h4>4.3 ★</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+        <div className="res-card">
+            <div className="image" style={{background:'#cfcfcf96'}}>
+                <div></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Restaurant Name</h2>
+                <p className="cuisine">Indian, Chinese, Beverages, Sodepur</p>
+                <h4>4.3 ★</h4>
+                <h4>30 minutes</h4>
+            </div>
+        </div>
+        </div>
+        </div>
+    </>   
+        )
+}
+export default Shimmer
